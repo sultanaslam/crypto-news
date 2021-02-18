4ba04c4d25fe27da34a8f5b155193f44f0d1f9ae
Sultan Aslam <sultanaslam93@gmail.com>
2021-02-18 18:16:02 +0500

    currency search

--- project/app/views.py
+++ project/app/views.py
@@ -1,10 +1,18 @@
+import json
+import requests
 from django.shortcuts import render
 
 def home(req):
-    import json
-    import requests
-    price_resp = requests.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH&tsyms=USD')
+    price_resp = requests.get('https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETC,XRP,BCH,EOS,LTC,XLM,ADA,USDT,TRX&tsyms=USD')
     price = json.loads(price_resp.content)
     api_resp = requests.get('https://min-api.cryptocompare.com/data/v2/news/?lang=EN')
     api = json.loads(api_resp.content)
     return render(req, 'home.html', { 'api': api, 'price':price })
+
+def prices(req):
+    crypto, quote = {}, ""
+    if req.method == 'POST':
+        quote = req.POST['quote'].upper()
+        crypto_resp = requests.get(f'https://min-api.cryptocompare.com/data/pricemultifull?fsyms={quote}&tsyms=USD')
+        crypto = json.loads(crypto_resp.content)
+    return render(req,'prices.html', {'quote': quote, "crypto": crypto})
