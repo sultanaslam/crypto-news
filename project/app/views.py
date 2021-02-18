09d650ca01fc8c336fb33f13c6232c85c5937b29
Sultan Aslam <sultanaslam93@gmail.com>
2021-02-18 17:19:02 +0500

    integrate crypto currency price

--- project/app/views.py
+++ project/app/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 def home(req):
     import json
     import requests
+    price_resp = requests.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH&tsyms=USD')
+    price = json.loads(price_resp.content)
     api_resp = requests.get('https://min-api.cryptocompare.com/data/v2/news/?lang=EN')
     api = json.loads(api_resp.content)
-    return render(req, 'home.html', { 'api': api })
+    return render(req, 'home.html', { 'api': api, 'price':price })
