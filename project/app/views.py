ba5ad945d3be275f713af955137efbb162e5f9e6
Sultan Aslam <sultanaslam93@gmail.com>
2021-02-18 16:49:04 +0500

    cruptocompare api integrated

--- project/app/views.py
+++ project/app/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 
 def home(req):
-    return render(req, 'home.html')
+    import json
+    import requests
+    api_resp = requests.get('https://min-api.cryptocompare.com/data/v2/news/?lang=EN')
+    api = json.loads(api_resp.content)
+    return render(req, 'home.html', { 'api': api })
